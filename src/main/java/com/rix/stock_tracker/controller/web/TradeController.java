8cd1b8d3ddfc4a358af7391a8db93c14af6a0f18
Rix <80359407+Rix1113@users.noreply.github.com>
2021-12-15 13:13:29 +0200

    [add-trade] - add method

--- src/main/java/com/rix/stock_tracker/controller/web/TradeController.java
+++ src/main/java/com/rix/stock_tracker/controller/web/TradeController.java
@@ -40,7 +40,7 @@ public class TradeController {
         return "redirect:/trade/all-trades";
     }
 
-    @PostMapping("/add-trade")
+    @GetMapping("/add-trade")
     public String showAddTradeForm(Model model) {
         model.addAttribute("trade", new Trade());
         return "trades/add-trade";
